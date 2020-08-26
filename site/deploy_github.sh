4acffeb95875ac23493f765e56ed4a72bd69423b
An Tran <tran.binhan@gmail.com>
2020-08-26 19:22:57 +0800

    Update deploy script

--- site/deploy_github.sh
+++ site/deploy_github.sh
@@ -9,9 +9,13 @@ echo "\033[0;32mDeleting old content...\033[0m"
 rm -rf $directory/*
 
 #-----
+echo "\033[0;32mChecking out $branch....\033[0m"
+git worktree add $directory $branch
+
+#-----
 echo "\033[0;32mGenerating site...\033[0m"
 
-pushd site/codelabs
+pushd codelabs
 claat export *.md
 popd
 
@@ -21,7 +25,6 @@ gulp dist --codelabs-dir=codelabs
 #-----
 echo "\033[0;32mDeploying $branch branch...\033[0m"
 
-git worktree add $directory $branch
 cd $directory &&
     echo "labs.swiftvietnam.com" > CNAME &&
     git pull origin $branch &&
